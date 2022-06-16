70e00fd10a4543bf603cc21b3ad1eb6d05cc178c
himkt <himkt@klis.tsukuba.ac.jp>
2022-06-16 23:32:37 +0900

    Get current filename

--- rplugin/python3/cargo_atcoder.py
+++ rplugin/python3/cargo_atcoder.py
@@ -1,15 +1,26 @@
+from pathlib import Path
 import os
 
 import pynvim
+import pynvim.api
 
 
 @pynvim.plugin
 class Main:
-    def __init__(self, nvim):
+    def __init__(self, nvim: pynvim.api.Nvim):
         self.nvim = nvim
 
     @pynvim.command("CargoAtCoderOpen", sync=True)
     def open_atcoder_problem_page(self):
-        print(type(self.nvim))
+        current_file_path_str: str = self.nvim.command_output('echo expand("%")')  # type: ignore
+        if current_file_path_str == "":
+            return
+
+        current_file_path = Path(current_file_path_str)
+        current_file_name = current_file_path.stem
+        self.nvim.command(f'echo "{current_file_name}"')
+
+        working_dir = os.getcwd()
+        self.nvim.command(f'echo "{working_dir}"')
         # self.nvim.command('silent !open https://atcoder.jp')
-        self.nvim.command('!open https://atcoder.jp')
+        # self.nvim.command('!open https://atcoder.jp')
